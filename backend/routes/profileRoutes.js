48f9064637a83d6a8fcf241013be97b5f27f55f1
jmpwew <162031609+jmpwew@users.noreply.github.com>
2026-05-09 02:44:33 +0800

    Delete backend directory

--- backend/routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const upload = require('../config/multer');
-const { updateProfile } = require('../controllers/profileController');
-
-router.put('/', upload.single('image'), updateProfile);
-
-module.exports = router;
